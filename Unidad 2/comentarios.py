c73aad7c55f87e538a41ab9dd278778907e711d7
jimenezpachecosinai-hash <jimenezpachecosinai@gmail.com>
2025-09-17 07:32:52 -0500

    comentarios

---
+++ Unidad 2/comentarios.py
@@ -0,0 +1,11 @@
+#Este programa imprime la variable.Tambien observamos la mutabilidad de python
+
+entero =10
+flotante =3.14
+cadena="Sinai Jmnz"
+booleana=True
+
+print(entero)
+print(flotante)
+print(cadena)
+print(booleana)
