d5bb11839eda8269a43c577905e4eac390890d21
jimenezpachecosinai-hash <jimenezpachecosinai@gmail.com>
2025-09-18 07:51:58 -0500

    Create lista1.py

---
+++ Unidad 2/lista1.py
@@ -0,0 +1,14 @@
+#Llamando las variables
+lista1 = ["Nat","Fati","Leo","Chris"]
+print(lista1)
+#Seleccionando la variable de la lista
+print(lista1[2])
+#Agregando un elemento a la lista
+lista1.append("Alexa")
+print(lista1)
+lista1.insert (1,"Sinai")
+print(lista1)
+#Insertando una de las variables
+lista1.pop()
+lista1.remove("Nat")
+lista1.pop(1)
